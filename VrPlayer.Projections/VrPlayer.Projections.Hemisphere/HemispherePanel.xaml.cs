21743d4a12becec45c1d9641dde07c9ee61a6c08
bmolyneaux <bmolyneaux@Ben-PC>
2014-01-24 14:20:41 -0500

    Add Hemisphere projection for 180 degree fisheye media.

---
+++ VrPlayer.Projections/VrPlayer.Projections.Hemisphere/HemispherePanel.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace VrPlayer.Projections.Hemisphere
+{
+    public partial class HemispherePanel : UserControl
+    {
+        public HemispherePanel(HemisphereProjection projection)
+        {
+            InitializeComponent();
+            try
+            {
+                DataContext = projection;
+            }
+            catch (Exception exc)
+            {
+            }
+        }
+    }
+}
---
+++ VrPlayer.Projections/VrPlayer.Projections.Hemisphere/HemispherePlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using VrPlayer.Contracts;
+using VrPlayer.Contracts.Projections;
+using VrPlayer.Helpers;
+
+namespace VrPlayer.Projections.Hemisphere
+{
+    [Export(typeof(IPlugin<IProjection>))]
+    public class HemispherePlugin : PluginBase<IProjection>
+    {
+        public HemispherePlugin()
+        {
+            try
+            {
+                Name = "Hemisphere";
+                var projection = new HemisphereProjection();
+                Content = projection;
+                Panel = new HemispherePanel(projection);
+                InjectConfig(PluginConfig.FromSettings(ConfigHelper.LoadConfig().AppSettings.Settings));
+            }
+            catch (Exception exc)
+            {
+                Logger.Instance.Error(string.Format("Error while loading '{0}'", GetType().FullName), exc);
+            }
+        }
+    }
+}
