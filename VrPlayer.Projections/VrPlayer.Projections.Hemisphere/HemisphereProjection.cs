b8b41ccc8a957ce0b98bb91bfabdd0009ddc67b0
bmolyneaux <bmolyneaux@Ben-PC>
2014-03-17 22:08:23 -0400

    Add more configurability to Hemisphere projection

--- VrPlayer.Projections/VrPlayer.Projections.Hemisphere/HemisphereProjection.cs
+++ VrPlayer.Projections/VrPlayer.Projections.Hemisphere/HemisphereProjection.cs
@@ -14,7 +14,7 @@ namespace VrPlayer.Projections.Hemisphere
         private double _radius = 1;
         private const double Distance = 1000;
         private const double HorizontalCoverage = 0.5D;
-        
+
         public static readonly DependencyProperty SlicesProperty =
             DependencyProperty.Register("Slices", typeof(int),
             typeof(HemisphereProjection), new FrameworkPropertyMetadata(16));
@@ -45,34 +45,84 @@ namespace VrPlayer.Projections.Hemisphere
             set { SetValue(CoverageProperty, value); }
         }
 
-        public static readonly DependencyProperty FishEyeFOVProperty =
-            DependencyProperty.Register("FishEyeFOV", typeof(double),
+        public static readonly DependencyProperty FishEyeFOVHorizontalProperty =
+            DependencyProperty.Register("FishEyeFOVHorizontal", typeof(double),
             typeof(HemisphereProjection), new FrameworkPropertyMetadata(180D));
         [DataMember]
-        public double FishEyeFOV
+        public double FishEyeFOVHorizontal
         {
-            get { return (double)GetValue(FishEyeFOVProperty); }
-            set { SetValue(FishEyeFOVProperty, value); }
+            get { return (double)GetValue(FishEyeFOVHorizontalProperty); }
+            set { SetValue(FishEyeFOVHorizontalProperty, value); }
         }
 
-        public static readonly DependencyProperty HeadingProperty =
-            DependencyProperty.Register("Heading", typeof(int),
-            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        public static readonly DependencyProperty FishEyeFOVVerticalProperty =
+            DependencyProperty.Register("FishEyeFOVVertical", typeof(double),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(180D));
         [DataMember]
-        public int Heading
+        public double FishEyeFOVVertical
         {
-            get { return (int)GetValue(HeadingProperty); }
-            set { SetValue(HeadingProperty, value); }
+            get { return (double)GetValue(FishEyeFOVVerticalProperty); }
+            set { SetValue(FishEyeFOVVerticalProperty, value); }
         }
 
-        public static readonly DependencyProperty TiltProperty =
-            DependencyProperty.Register("Tilt", typeof(int),
+        public static readonly DependencyProperty RollLeftProperty =
+            DependencyProperty.Register("RollLeft", typeof(int),
             typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
         [DataMember]
-        public int Tilt
+        public int RollLeft
         {
-            get { return (int)GetValue(TiltProperty); }
-            set { SetValue(TiltProperty, value); }
+            get { return (int)GetValue(RollLeftProperty); }
+            set { SetValue(RollLeftProperty, value); }
+        }
+
+        public static readonly DependencyProperty RollRightProperty =
+            DependencyProperty.Register("RollRight", typeof(int),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        [DataMember]
+        public int RollRight
+        {
+            get { return (int)GetValue(RollRightProperty); }
+            set { SetValue(RollRightProperty, value); }
+        }
+
+        public static readonly DependencyProperty YLeftProperty =
+            DependencyProperty.Register("YLeft", typeof(int),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        [DataMember]
+        public int YLeft
+        {
+            get { return (int)GetValue(YLeftProperty); }
+            set { SetValue(YLeftProperty, value); }
+        }
+
+        public static readonly DependencyProperty YRightProperty =
+            DependencyProperty.Register("YRight", typeof(int),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        [DataMember]
+        public int YRight
+        {
+            get { return (int)GetValue(YRightProperty); }
+            set { SetValue(YRightProperty, value); }
+        }
+
+        public static readonly DependencyProperty XLeftProperty =
+            DependencyProperty.Register("XLeft", typeof(int),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        [DataMember]
+        public int XLeft
+        {
+            get { return (int)GetValue(XLeftProperty); }
+            set { SetValue(XLeftProperty, value); }
+        }
+
+        public static readonly DependencyProperty XRightProperty =
+            DependencyProperty.Register("XRight", typeof(int),
+            typeof(HemisphereProjection), new FrameworkPropertyMetadata(0));
+        [DataMember]
+        public int XRight
+        {
+            get { return (int)GetValue(XRightProperty); }
+            set { SetValue(XRightProperty, value); }
         }
 
         public Point3D Center
@@ -127,12 +177,13 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         double theta = HorizontalCoverage * slice * 4 * Math.PI / Slices + Math.PI * (0.5 - HorizontalCoverage);
+
                         double x = scale * Math.Sin(theta) + Radius;
                         double z = scale * Math.Cos(theta);
 
                         var normal = new Vector3D(x + Distance, y, z) + Center;
 
-                        var axis = new AxisAngleRotation3D(new Vector3D(1, 0, 0), Tilt + 90);
+                        var axis = new AxisAngleRotation3D(new Vector3D(1, 0, 0), 0 + 90);
                         var trans = new RotateTransform3D(axis, new Point3D(0, 0, 0));
                         var vector = Vector3D.Multiply(new Vector3D(normal.X, normal.Y, normal.Z), trans.Value);
                         positions.Add(new Point3D(vector.X, vector.Y, vector.Z));
@@ -149,23 +200,19 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         double theta = HorizontalCoverage * slice * 4 * Math.PI / Slices + Math.PI * (0.5 - HorizontalCoverage);
+
                         double x = scale * Math.Sin(theta) - Radius;
                         double z = scale * Math.Cos(theta);
 
                         var normal = new Vector3D(x - Distance, y, z) + Center;
 
-                        var axis = new AxisAngleRotation3D(new Vector3D(1, 0, 0), Tilt + 90);
+                        var axis = new AxisAngleRotation3D(new Vector3D(1, 0, 0), 0 + 90);
                         var trans = new RotateTransform3D(axis, new Point3D(0, 0, 0));
                         var vector = Vector3D.Multiply(new Vector3D(normal.X, normal.Y, normal.Z), trans.Value);
                         positions.Add(new Point3D(vector.X, vector.Y, vector.Z));
                     }
                 }
 
-                // Create triangle that will never be seen. It is a work around for automatic texture scaling in WPF.
-                //  It is needed so we can scale for Fisheye lens FOV greater than 180.
-                positions.Add(new Point3D(10000, 10000, 10000));
-                positions.Add(new Point3D(10000, 10001, 10000));
-                positions.Add(new Point3D(10000, 10000, 10001));
                 return positions;
             }
         }
@@ -216,11 +263,6 @@ namespace VrPlayer.Projections.Hemisphere
                     }
                 }
 
-                // Create triangle that will never be seen. It is a work around for automatic texture scaling in WPF.
-                //  It is needed so we can scale for Fisheye lens FOV greater than 180.
-                triangleIndices.Add((Slices+1)*(Stacks+1)+1);
-                triangleIndices.Add((Slices + 1) * (Stacks + 1) + 2);
-                triangleIndices.Add((Slices + 1) * (Stacks + 1) + 3);
                 return triangleIndices;
             }
         }
@@ -229,8 +271,10 @@ namespace VrPlayer.Projections.Hemisphere
         {
             get
             {
-                double fisheyeScale = 1.0;
-                if (FishEyeFOV > 180.0) fisheyeScale -= (FishEyeFOV - 180.0) / 180.0;
+                double fisheyeScaleHorizontal = 1.0;
+                double fisheyeScaleVertical = 1.0;
+                fisheyeScaleHorizontal -= (FishEyeFOVHorizontal - 180.0) / 180.0;
+                fisheyeScaleVertical -= (FishEyeFOVVertical - 180.0) / 180.0;
                 var textureCoordinates = new PointCollection();
 
                 //Left
@@ -239,8 +283,8 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.5 + fisheyeScale * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = 0.5 + fisheyeScaleHorizontal * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
+                        var v = 0.5 + fisheyeScaleVertical * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
@@ -251,17 +295,12 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.5 + fisheyeScale * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = 0.5 + fisheyeScaleHorizontal * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
+                        var v = 0.5 + fisheyeScaleVertical * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
 
-                // The extra triangle that will never be seen must have texture coords of 0,0 and 0,1 for two of the positions. It is a work around for automatic texture scaling in WPF.
-                //  It will keep WPF from scaling the texture after cropping for fisheye FOV setting greater than 180 degrees.
-                textureCoordinates.Add(new Point(0, 0));
-                textureCoordinates.Add(new Point(0, 1));
-                textureCoordinates.Add(new Point(1, 1));
                 return textureCoordinates;
             }
         }
@@ -271,7 +310,7 @@ namespace VrPlayer.Projections.Hemisphere
             get
             {
                 double fisheyeScale = 1.0;
-                if (FishEyeFOV > 180.0) fisheyeScale -= (FishEyeFOV - 180.0) / 180.0;
+                if (FishEyeFOVHorizontal > 180.0) fisheyeScale -= (FishEyeFOVHorizontal - 180.0) / 180.0;
                 var textureCoordinates = new PointCollection();
 
                 //Left
@@ -280,8 +319,8 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.25 + fisheyeScale * (p.Y / 4) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
+                        var v = 0.25 + fisheyeScale * (p.Y / 4) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
@@ -292,17 +331,12 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.75 + fisheyeScale * (p.Y / 4) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = 0.5 + fisheyeScale * (p.Y / 2) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
+                        var v = 0.75 + fisheyeScale * (p.Y / 4) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
 
-                // The extra triangle that will never be seen must have texture coords of 0,0 and 0,1 for two of the positions. It is a work around for automatic texture scaling in WPF.
-                //  It will keep WPF from scaling the texture after cropping for fisheye FOV setting greater than 180 degrees.
-                textureCoordinates.Add(new Point(0, 0));
-                textureCoordinates.Add(new Point(0, 1));
-                textureCoordinates.Add(new Point(1, 1));
                 return textureCoordinates;
             }
 
@@ -312,8 +346,10 @@ namespace VrPlayer.Projections.Hemisphere
         {
             get
             {
-                double fisheyeScale = 1.0;
-                if (FishEyeFOV > 180.0) fisheyeScale -= (FishEyeFOV - 180.0) / 180.0;
+                double fisheyeScaleHorizontal = 1.0;
+                double fisheyeScaleVertical = 1.0;
+                fisheyeScaleHorizontal -= (FishEyeFOVHorizontal - 180.0) / 180.0;
+                fisheyeScaleVertical -= (FishEyeFOVVertical - 180.0) / 180.0;
                 var textureCoordinates = new PointCollection();
 
                 //Left
@@ -322,8 +358,8 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.25 + fisheyeScale*(p.Y / 4) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.5 + fisheyeScale*(p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = XLeft / 180.0 + 0.25 + fisheyeScaleHorizontal * (p.Y / 4) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
+                        var v = YLeft / 180.0 + 0.5 + fisheyeScaleVertical * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollLeft / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
@@ -334,17 +370,11 @@ namespace VrPlayer.Projections.Hemisphere
                     for (int slice = 0; slice <= Slices; slice++)
                     {
                         var p = new Point((double)slice / Slices, (double)stack / Stacks);
-                        var u = 0.75 + fisheyeScale*(p.Y / 4) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
-                        var v = 0.5 + fisheyeScale*(p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * Heading / 360);
+                        var u = XRight / 180.0 + 0.75 + fisheyeScaleHorizontal * (p.Y / 4) * Math.Cos(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
+                        var v = YRight / 180.0 + 0.5 + fisheyeScaleVertical * (p.Y / 2) * Math.Sin(p.X * 2 * Math.PI - Math.PI / 2 - 2 * Math.PI * RollRight / 360);
                         textureCoordinates.Add(new Point(u, v));
                     }
                 }
-
-                // The extra triangle that will never be seen must have texture coords of 0,0 and 0,1 for two of the positions. It is a work around for automatic texture scaling in WPF.
-                //  It will keep WPF from scaling the texture after cropping for fisheye FOV setting greater than 180 degrees.
-                textureCoordinates.Add(new Point(0, 0));
-                textureCoordinates.Add(new Point(0, 1));
-                textureCoordinates.Add(new Point(1, 1));
 
                 return textureCoordinates;
             }
